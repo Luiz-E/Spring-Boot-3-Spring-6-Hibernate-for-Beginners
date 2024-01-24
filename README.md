470f277726a558de718ce4764eebc9bbb76f658d
Luiz_E <luizedumartins21@gmail.com>
2024-01-23 21:56:01 -0300

    Revert "introduction to beans scopes"
    This reverts commit cf50fbbc

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-This project is used as an introduction to bean lifecycle methods
-
-Spring doesn't call the destroy method of beans in prototype scope
-Prototype beans are, by default, lazy, so there's no need to add the annotation
+This project is used as an introduction to lazy initialization
--- src/main/java/com/luizedu/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luizedu/springcoredemo/common/CricketCoach.java
@@ -1,10 +1,10 @@
 package com.luizedu.springcoredemo.common;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
@@ -13,16 +13,6 @@ public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes.";
-    }
-
-    @PostConstruct
-    public void starting() {
-        System.out.println("Starting..." + getClass().getSimpleName());
-    }
-
-    @PreDestroy
-    public void destroying() {
-        System.out.println("Finishing..." + getClass().getSimpleName());
     }
 
 }
